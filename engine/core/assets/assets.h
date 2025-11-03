fdc191c91764e965ff5f595cd6505d01c4ef16a6
carlosyzy <15732633043@163com>
2025-11-03 23:02:29 +0800

    fix：资产管理完善

--- engine/core/assets/assets.h
+++ engine/core/assets/assets.h
@@ -1,8 +1,41 @@
 #pragma once
+enum class AssetType
+{
+    None,
+    Image,
+    Audio,
+    Font,
+    Shader,
+    Material,
+};
+struct AssetPath
+{
+    /**
+     * @brief 资产路径
+     * @param path 绝对路径
+     */
+    std::string path;
+    /**
+     * @brief 资产类型
+     */
+    AssetType type;
+};
+
 class Assets
 {
 private:
+    /**
+     * @brief 资产根目录
+     */
     std::string _root;
+    /**
+     * @brief 资产映射表
+     */
+    std::unordered_map<AssetPath, std::string> _assetsMap;
+    /**
+     * @brief 最大加载数量
+     */
+    int MAX_LOAD_COUNT = 30;
     void _initRoot();
 public:
 	Assets();
