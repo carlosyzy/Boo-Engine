462c253246d425149a77ee807b681efc1a9ae973
穿越的杨宗宝 <15732633043@163.com>
2025-12-21 21:11:58 +0800

    fix:节点树悬浮，选中状态更新

--- editor/main/hierarchy/editor-hierarchy-node-tree.h
+++ editor/main/hierarchy/editor-hierarchy-node-tree.h
@@ -5,6 +5,7 @@
 #include <cstdint>
 #include "../../../engine/core/component/component-register.h"
 #include "../../../engine/core/component/component.h"
+#include "../../../engine/core/input/input-struct.h"
 
 class Node;
 class Node2D;
@@ -22,8 +23,9 @@ struct NodeTreeStructure
     /**层级 */
     int layer = -1;
     /** 节点在树中的索引 */
+    std::string icon = "";
     int index;
-    /** 节点-节点树时使用 */
+    // 节点树中的显示节点
     Node2D *ndBind;
     /** 子节点 */
     std::vector<NodeTreeStructure> children;
@@ -31,6 +33,12 @@ struct NodeTreeStructure
 
 class EditorHierarchyNodeTree : public Component
 {
+private:
+    int _contentTouchID = -1;
+    int _contentHoverID = -1;
+    void _onTreeContentTouchEvent(NodeInputResult &result);
+    void _onTreeContentHoverEvent(NodeInputResult &result);
+
 private:
     float _topLen;
     float _leftLen;
@@ -42,29 +50,31 @@ private:
     float _contentY;
     void _initContent();
 
+private:
     Node *_rootNode = nullptr;
-    Scene *_rootScene = nullptr; // 改为弱引用，不拥
-
-    std::unordered_map<std::string, Node *> _nodes; // 节点-节点树item 映射
-
-
+    Scene *_rootScene = nullptr;   // 改为弱引用，不拥
+    NodeTreeStructure _uiTreeData; // 节点树数据
     int _nodeIndex;
+    float _itemHeight = 20.0f;
     std::vector<Node2D *> _nodePools;
-    NodeTreeStructure *_hoverTreeItem;
+    std::vector<Node2D *> _treeNodes; // 节点-节点树item 映射
+    std::map<std::string, NodeTreeStructure *> _treeNodeDataMap;
+    /** 是否需要刷新 */
+    bool _isDirty = true;
     void _setTrees(Node *root, NodeTreeStructure &uiTreeData, int layer);
     void _updateTreeContent();
     void _updateTreesItems(NodeTreeStructure &uiTreeData);
-    void _onTreeItemTouchEvent(NodeTreeStructure &result);
-    void _onTreeItemCursorHoverEvent(NodeTreeStructure &result);
 
-    // 当前显示的节点树映射
-    std::map<std::string, NodeTreeStructure *> _treeUIMap;
-     // item 折叠按钮的touch 事件
-    std::map<std::string, NodeTreeStructure *> _foldUIMap; // 折叠按钮-节点树item 映射
-    /** 是否需要刷新 */
-    bool _isDirty = true;
-    // 节点树数据
-    NodeTreeStructure _uiTreeData;
+private:
+    NodeTreeStructure *_hoverTreeItem = nullptr;
+    NodeTreeStructure *_selectTreeItem = nullptr;
+    /**
+     * 刷新节点树item的UI
+     * @param ndItem 节点树item
+     * @param state 刷新状态 0-正常 1-选中 2-悬浮
+     */
+    void _refreshTreeItemState(Node2D *ndItem, int state);
+    void _refreshTreeItemUI(NodeTreeStructure *tree, int state);
 
 protected:
     void _deserialized() override;
---
+++ engine/core/input/input-struct.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <map>
+
+class Node;
+class Node2D;
+
+
+struct NodeInputResult
+{
+	Node2D *node = nullptr;
+	float localX = 0;
+	float localY = 0;
+	float worldX = 0;
+	float worldY = 0;
+	int button = 0;
+};
+enum class NodeInput
+{
+	TOUCH_START,
+	TOUCH_MOVE,
+	TOUCH_END,
+	TOUCH_CANCEL,
+	CURSOR_HOVER,
+};
+
+/**
+ * 节点的输入事件
+ * @brief 节点输入结构体
+ */
+struct NodeInputStruct
+{
+
+	Node2D *node = nullptr;
+
+	bool isIntercept = false;
+
+	int status = 0;
+
+	NodeInputResult touchResult{};
+
+	std::map<int, std::function<void(NodeInputResult &)>> touchStarts;
+	std::map<int, std::function<void(NodeInputResult &)>> touchMoves;
+	std::map<int, std::function<void(NodeInputResult &)>> touchEnds;
+	std::map<int, std::function<void(NodeInputResult &)>> touchCancels;
+	std::map<int, std::function<void(NodeInputResult &)>> cursorHovers;
+};
